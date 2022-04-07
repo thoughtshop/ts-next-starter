79de2917cd354089c5b4979f0ff3b031aabbef59
Corey Lowe <corey@thoughtshop.com>
2022-04-07 14:51:37 -0700

    Update Head.jsx

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -3,31 +3,41 @@
  * @param {boolean} props.robots
  * @param {string} props.pageDesc
  * @param {string} props.pageTitle
- * @param {string} props.seoImage
+ * @param {string} props.seoImage path to SEO image
  */
-const Head = (props) => (
+const Head = ({ pageDesc, pageTitle, robots, seoImage }) => (
   <>
-    <meta content='IE=edge' httpEquiv='X-UA-Compatible' />
-    <meta name='robots' content={props.robots ? 'index,follow' : 'none'} />
-    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    <meta name='description' content={props.pageDesc} />
+    <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+    <meta name="robots" content={robots ? 'index,follow' : 'none'} />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content={pageDesc} />
 
-    <title>{props.pageTitle}</title>
+    <title>{pageTitle}</title>
 
-    <link href='./assets/images/favicon-32x32.png' rel='icon' sizes='32x32' type='image/png' />
-    <link href='./assets/images/favicon-16x16.png' rel='icon' sizes='16x16' type='image/png' />
+    <link
+      href="/images/favicon-32x32.png"
+      rel="icon"
+      sizes="32x32"
+      type="image/png"
+    />
+    <link
+      href="/images/favicon-16x16.png"
+      rel="icon"
+      sizes="16x16"
+      type="image/png"
+    />
 
-    <link href={props.seoImage} rel='image_src' />
+    <link href={seoImage} rel="image_src" />
 
-    <meta property='og:title' content={props.pageTitle} />
-    <meta property='og:description' content={props.pageDesc} />
-    <meta property='og:url' content={props.pageUrl} />
-    <meta property='og:image' content={props.seoImage} />
+    <meta property="og:title" content={pageTitle} />
+    <meta property="og:description" content={pageDesc} />
+    <meta property="og:url" content={pageUrl} />
+    <meta property="og:image" content={seoImage} />
 
-    <meta name='twitter:card' content='summary_large_image' />
-    <meta name='twitter:title' content={props.pageTitle} />
-    <meta name='twitter:description' content={props.pageUrl} />
-    <meta name='twitter:image' content={props.seoImage} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:title" content={pageTitle} />
+    <meta name="twitter:description" content={pageUrl} />
+    <meta name="twitter:image" content={seoImage} />
   </>
 );
 
