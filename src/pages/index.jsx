df05739f6d27efba39fb910a1179e7163ef5ca08
Corey Lowe <corey@thoughtshop.com>
2022-04-07 14:05:23 -0700

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,17 @@
 import NextHead from 'next/Head';
-import { Head, Nav, Footer } from '../components';
+
+import Head from '../components/Head';
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 
 const Home = () => (
   <div>
     <NextHead>
       <Head
         robots={false}
-        pageTitle='A thoughtful title'
-        pageDesc='A thoughtful description'
-      ></Head>
+        pageTitle="A thoughtful title"
+        pageDesc="A thoughtful description"
+      />
     </NextHead>
     <Nav />
     <main></main>
